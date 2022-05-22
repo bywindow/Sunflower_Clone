aeafe869ddde85ca7ee39ce41dbef126f60f6028
dionidip <paulus0617@gmail.com>
2022-05-23 02:06:01 +0900

    ➕ build: make MainApplication class and add dependencies

--- app/src/main/java/com/example/sunflower_clone/MainApplication.kt
+++ app/src/main/java/com/example/sunflower_clone/MainApplication.kt
@@ -1,6 +1,13 @@
 package com.example.sunflower_clone
 
 import android.app.Application
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
 
-class MainApplication : Application(){
+@HiltAndroidApp
+class MainApplication : Application(), Configuration.Provider {
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder()
+            .setMinimumLoggingLevel(if (BuildConfig.DEBUG) android.util.Log.DEBUG else android.util.Log.ERROR)
+            .build()
 }
