f4b53f551660bd759a9e69313ba572f094d045d6
dionidip <paulus0617@gmail.com>
2022-06-02 12:32:37 +0900

    Add: dataBinding in GrardenActivity

--- app/src/main/java/com/example/sunflower_clone/GardenActivity.kt
+++ app/src/main/java/com/example/sunflower_clone/GardenActivity.kt
@@ -1,7 +1,15 @@
 package com.example.sunflower_clone
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.sunflower_clone.databinding.ActivityGardenBinding
 
 
-class GardenActivity {
-  
+class GardenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        DataBindingUtil.setContentView<ActivityGardenBinding>(this, R.layout.activity_garden)
+    }
 }
